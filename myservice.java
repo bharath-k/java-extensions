d1242a5459354bb8c487f7c0057e6ed3027a3a58
Bharath K <bharath.kumaran@deshaw.com>
2015-05-07 21:07:27 +0530

    I screwed up the merge commit. Must use branches.

---
+++ myservice.java
@@ -0,0 +1,15 @@
+package org.me;
+
+public class myservice {
+    private String m_stringEntry;
+
+    public String getstringEntry()
+    {
+        return m_stringEntry;
+    }
+
+    public void setstringEntry(String entry)
+    {
+        this.m_stringEntry = entry;
+    }
+}
